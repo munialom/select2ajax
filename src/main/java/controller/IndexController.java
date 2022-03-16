c0bcef10009e83c2d9985a0f1e39f146386aa2f8
Moses Munialo <munialom@gmail.com>
2022-03-16 13:05:06 +0300

    moses |  added list method

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -1,5 +1,10 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ctecx.select2ajax.model.Employee;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +15,21 @@ public class IndexController {
     @GetMapping(value="/index")
     public String index () {
         return"/index";
+    }
+
+    public List<String> listofEmployees(){
+        
+ 
+        List<String> emp= new ArrayList<String>();
+        emp.add("moses");
+        emp.add("moses");
+        emp.add("moses");
+        emp.add("moses");
+        emp.add("moses");
+        emp.add("moses");
+        
+
+       return emp;  
     }
     
     
